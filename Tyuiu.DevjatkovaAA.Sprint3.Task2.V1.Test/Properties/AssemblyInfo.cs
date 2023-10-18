ca44e10099f0934ae083a10589500670a1dc5936
Tyuiu.DevjatkovaAA@outlook.com <annastaia1414@gmail.com>
2023-10-18 09:29:17 +0500

    Task2

---
+++ Tyuiu.DevjatkovaAA.Sprint3.Task2.V1.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.DevjatkovaAA.Sprint3.Task2.V1.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.DevjatkovaAA.Sprint3.Task2.V1.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ff1fcab4-a147-4453-b37c-0581a9d2f475")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.DevjatkovaAA.Sprint3.Task2.V1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.DevjatkovaAA.Sprint3.Task2.V1.Lib;
+
+namespace Tyuiu.DevjatkovaAA.Sprint3.Task2.V1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнила: Девяткова А. А. | АСОиУБ-23-3";
+            //Длина строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Оператор цикла do..while.                                         *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #1                                                              *");
+            Console.WriteLine("* Выполнила: Девяткова Анастасия Александровна | АСОиУБ-23-3              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, используя цикл do..while, которая вычисляет сумму   *");
+            Console.WriteLine("* ряда по формуле:                                                        *");
+            Console.WriteLine("*                 2                                                       *");
+            Console.WriteLine("*  S = sin i * 0,5                                                        *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("* при a = 1                                                               *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            double value = 1;
+            int startValue = 1;
+            int stopValue = 10;
+
+            Console.WriteLine("Переменная a = " + value);
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            double res = ds.GetSumSeries(value, startValue, stopValue);
+
+            Console.WriteLine("S = " + res);
+
+            Console.ReadKey();
+        }
+    }
+}
